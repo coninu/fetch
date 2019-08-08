bba59a8f63a0711e31b4bec4dacd36549c40b6a1
Coni <coniarganaraz@hotmail.com>
2019-08-08 20:34:58 -0300

    avenida siempre viva

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
-fetch('https://reqres.in/api/user')
+/*fetch('https://reqres.in/api/user')
     .then(response => response.json())
     .then(res => {
         // console.log(res.data);
         const {data} = res
         data.map(({id, name}) => console.log(`id: ${id}\nname: ${name}`))
-    })
+    })*/
+
+fetch('https://raw.githubusercontent.com/coninu/fetch/master/cv.json')
+.then(response => response.json())
+.then (res => {
+ console.log();
+})
